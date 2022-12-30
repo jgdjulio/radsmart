24675d77c5bf4490636671ab5b86cadc4df69731
juliogd <jgdjulio@gmail.com>
2022-12-29 22:12:20 -0300

    update

---
+++ appengine_config.py
@@ -0,0 +1,11 @@
+from google.appengine.ext import vendor
+
+# Add any libraries installed in the "lib" folder.vendor.add('lib')
+cs50
+Flask
+Flask-Session
+requests
+Jinja2
+virtualenv
+Werkzeug
+tablib
